acc94db58dd0fc6e33f35c196f83e78b66cbb9e6
Lucas Varotto <lucas.varotto01@gmail.com>
2021-12-09 21:11:22 -0800

    FINISHED DAY 10 IN 11 MINUTES

--- Day 10 Part 1.py
+++ Day 10 Part 1.py
@@ -3,3 +3,46 @@ import helper
 with open(helper.nrml("day10.txt")) as f:
     contents = f.read().split("\n")
 
+mapped = {
+    "(": ")",
+    "<": ">",
+    "{": "}",
+    "[": "]"
+}
+
+worth = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137
+}
+
+inc = []
+
+summed = 0
+for item in contents:
+    x = []
+    go = True
+    for c in item:
+        if c in mapped.keys():
+            x.append(c)
+        if c in mapped.values():
+            if c == mapped[x[-1]]:
+                del x[-1]
+            else:
+                summed += worth[c]
+                go = False
+                break
+    if go:
+        rev = [mapped[y] for y in x[::-1]]
+        
+        s = 0
+        for y in rev:
+            s = (s * 5) + {
+                    ")": 1,
+                    "]": 2,
+                    "}": 3,
+                    ">": 4
+                }[y]
+        inc.append(s)
+print(sorted(inc)[len(inc) // 2])
