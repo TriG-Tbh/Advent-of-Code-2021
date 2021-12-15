be2e16cb9fdfd6ab9bdc0e895ebb2ea0ddf0bb04
Lucas Varotto <lucas.varotto01@gmail.com>
2021-12-15 06:44:39 -0800

    at this point idk what im doing

--- Day 15 Part 1.py
+++ Day 15 Part 1.py
@@ -9,28 +9,44 @@ def validate(point, path):
     y, x = point
     return (0 <= y < len(contents)) and (0 <= x < len(contents[0])) and (point not in path)
 
-def branch(point, path, depth=0):
-    #if depth == 0:
-    #print(depth)
-    y, x = point
-    if point not in path:
-        path.append(point)
-    valid2 = [p for p in [(y - 1, x), (y, x + 1), (y + 1, x), (y, x - 1)] if validate(p, path.copy())]
-    #print(valid2)   
-    valid2.sort(key=lambda i: int(contents[i[0]][i[1]]))
-    #print(valid2)
-    #input()
-    for p in valid2:
-        
-        py, px = p
-        if py == len(contents) - 1 and px == len(contents[0]) - 1:
-            pathnum = [contents[zy][zx] for zy, zx in path]
-            included["".join(pathnum)] = sum([int(f) for f in pathnum])
-            #print(pathnum)
-            break
-        else:
-            branch(p, path.copy(), depth=depth+1)
 
-branch((0, 0), [(0, 0)])
+
+distances = {}
+for y in range(len(contents)):
+    for x in range(len(contents[0])):
+        if y == x == 0:
+            distances[(y, x)] = 0
+        else:
+            distances[(y, x)] = 999999999999999999
+
+cd = 0
+current = ()
+
+def validate(point):
+    y, x = point
+    return (0 <= y < len(contents)) and (0 <= x < len(contents[0])) and point in unvisited
+
+while (len(contents) - 1, len(contents[0]) - 1) in unvisited:
+
+    y, x = current
+    valid = list(filter(validate, [(y - 1, x), (y, x + 1), (y + 1, x), (y, x - 1)]))
+    point_distance = {}
+    for p in valid:
+        py, px = p
+        newd = distances[current] + int(contents[py][px])
+        if newd < distances[p]:
+            distances[p] = newd
+        point_distance[p] = distances[p]
+    
+    points = list(point_distance.keys())
+    points.sort(key=lambda x: point_distance[x])
+    del unvisited[current]
+    #print(points)
+    if not unvisited:
+        break
+    current = points[0]
+
+
+        
 
 print(included)
