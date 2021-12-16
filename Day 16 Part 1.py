5ddfe3a3c5d6e73bceb4014afd601dc40dd410e8
Lucas Varotto <lucas.varotto01@gmail.com>
2021-12-15 21:54:41 -0800

    optimized snowman, started day 15

--- Day 16 Part 1.py
+++ Day 16 Part 1.py
@@ -1,3 +1,55 @@
 import helper
 with open(helper.nrml("day16.txt")) as f:
-    contents = f.read().splitlines()
+    contents = f.read()
+
+mapped = {'0': "0000", '1': "0001", '2': "0010", '3': "0011", '4': "0100", '5': "0101", '6': "0110", '7': "0111", '8': "1000", '9': "1001",  'A': "1010", 'B': "1011", 'C': "1100", 'D': "1101", 'E': "1110", 'F': "1111"}
+
+new = ""
+for item in contents:
+    new += mapped[item]
+
+versions = []
+while new != "":
+
+    lenbits = 0
+    lenhex = 0
+
+    version = int(new[:3], 2)
+    new = new[3:]
+    versions.append(version)
+    
+    lenbits += 3
+
+    typeid = int(new[:3], 2)
+    new = new[3:]
+    lenbits += 3
+    
+    if typeid == 4:
+        ppart = "1xxxx"
+        total = ""
+        while ppart[0] == "1":
+            ppart = new[:5]
+            total = total + ppart
+            new = new[5:]
+            lenbits += 5
+        ppart = new[:5]
+        total = total + ppart
+        new = new[5:]
+        lenbits += 5
+
+        new = new[(4 - lenbits % 4):]
+    else:
+        ltid = new[:1]
+        new = new[1:]
+
+        lenbits += 1
+        length = ""
+        if ltid == "1":
+            length = new[:15]
+            new = new[15:]
+            lenbits += 15
+        else:
+            length = new[:11]
+            new = new[11:]
+            lenbits += 11
+        
--- snowman.py
+++ snowman.py
@@ -27,73 +27,72 @@ def snowman(n: int, arr: list) -> tuple:
     ---
     snowman(3, [["0006060066"], ["6900696600"], ["0000990006"], ["6060906606"]]) -> ((0, 6), (2, 9))
     """
+    global flattened
     if isinstance(arr[0][0], int):
-        arr = [[str(x) for x in line] for line in arr]
+        flattened = [str(x) for line in arr for x in line]
+        length = len(arr[0])
+        height = len(arr)
     elif len(arr[0]) == 1:
-        arr = [[x for x in str(line[0])] for line in arr]
+        flattened = [x for line in arr for x in str(line[0])]
+        height = len(arr)
+        length = len(arr[0][0])
+    else:
+        flattened = [x for line in arr for x in line]
+        length = len(arr[0])
+        height = len(arr)
 
-    length = len(arr[0])
-    height = len(arr)
-
-    def validate(point):
-        y, x = point
-        return (0 <= y < height) and (0 <= x < length)
+    arr = []
 
     def explode(point):
+        global flattened
         y, x = point
         
-        valid2 = list(filter(validate, [(y - 1, x), (y, x + 1), (y + 1, x), (y, x - 1)]))
-        arr[y][x] = "X"
-        flattened = [x for line in arr for x in line]
+        valid2 = list(filter(lambda newpoint: (0 <= newpoint[0] < height) and (0 <= newpoint[1] < length), [(y - 1, x), (y, x + 1), (y + 1, x), (y, x - 1)]))
+        flattened[(point[0] * length) + point[1]] = "X"
         for p in valid2:
-            py, px = p
-            val = arr[py][px]
+            val = flattened[(p[0] * length) + p[1]]
             if val == "9":
                 explode(p)
-                flattened = [x for line in arr for x in line]
                 continue
             if flattened.count(val) > 1:
-                arr[py][px] = "X"
-        flattened = [x for line in arr for x in line]
+                flattened[(p[0] * length) + p[1]] = "X"
         valid2 = []
 
     i = 0
-    flattened = [x for line in arr for x in line] # removing this line causes runtime spike by 2+ seconds idk why or how
-
+    
     size = length * height
     while True:
         p = 0
         while p < n:
-            px = i % length
-            py = i // length
-            i += 1
+            
             
             if i >= size:
                 i -= size + 1
                 continue
-            if arr[py][px] != "X":
+            if flattened[i] != "X":
                 p += 1
-
-        val = arr[py][px]
+            if p < n:
+                i += 1
+            
+        px = i % length
+        py = i // length
+        val = flattened[(length * py) + px]
         if val == "9":
             explode((py, px))
+            continue
+        if flattened.count(val) > 1:
+            flattened[(length * py) + px] = "X"
         else:
-            flattened = [x for line in arr for x in line]
-            if flattened.count(val) > 1:
-                arr[py][px] = "X"
-            else:
-                break
+            break
     
-    arr = []
 
     for item in ["0", "6"]:
         while flattened.count(item) > 1:
-            i = "".join(flattened).rindex(item)
-            #i = (len(flattened) - 1 - i)
-            #arr[i // length][i % length] = "X"
-            flattened[i] = "X"
+            flattened["".join(flattened).rindex(item)] = "X"
 
     inside = tuple((flattened.index(item) // length, flattened.index(item) % length) if item in flattened else None for item in ["0", "6"])
     # tuple comprehension for the win lmao
+    
+    flattened = []
+    
     return inside
-
